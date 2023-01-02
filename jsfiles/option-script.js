1113c6f833cb392cb8dcf1527664621f2e52d262
Animesh <animeshganai0000@gmail.com>
2023-01-02 09:46:26 +0530

    Experiment 2

--- jsfiles/option-script.js
+++ jsfiles/option-script.js
@@ -1,4 +1,4 @@
-const serverAddress = "https://famous-wasp-mittens.cyclic.app/";
+const serverAddress = "wss://famous-wasp-mittens.cyclic.app/";
 const wss = new WebSocket(serverAddress);
 wss.binaryType = "arraybuffer";
 var rowIndexPuzzle;
